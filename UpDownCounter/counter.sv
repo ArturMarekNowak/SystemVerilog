7ea95357dd1ca5efbe818b62c465c42b65e69553
Artur Nowak <arturmareknowal@gmail.com>
2020-07-11 06:01:59 +0200

    Counter design and testbench improvemnts

--- UpDownCounter/counter.sv
+++ UpDownCounter/counter.sv
@@ -1,26 +1,52 @@
-module counter (  input clk,               
-                  input ctrl,
-                  input rstn,              
-                  output reg[2:0] out);    
+interface counter_if(input bit clk); //Declaration of counters interface with clock signal as input
+    logic [2:0] in, out;             //Declaration of signal in - value loaded into counter if signal load == 1 and an output register
+    logic rst, load, is_up;          //Declaration of reset signal, load control signal (if load == 1 value is loaded into counter)
+endinterface                         //Declaration of is_up control signal (if is_up == 1 counter counts up)
 
-  initial begin
-  
-  out <= 0;
-  
-  end
-  
-  
-  always @ (posedge clk or negedge clk) begin
 
-      if (rstn)
-        out <= 0;
-      else    
-        if (ctrl)
-            out <= out - 1;
-        else 
-           out <= out + 1;
-      end
- 
+module counter_with_ifc(counter_if counterif); //Declaration of counter with an interface
+
+initial begin              //Signals initial values
+    counterif.rst <= 0;
+    counterif.load <= 0;
+    counterif.in <= 0;
+    counterif.out <= 0;
+    counterif.is_up <= 0;
+end
+
+    //Modeling sequntional flipflop logic
+    always_ff @(posedge counterif.clk or posedge counterif.rst)
+        begin
+            if(counterif.rst)    //If reset is on outpu equals 0 
+                counterif.out <= '0;
+            else
+                if(counterif.load && counterif.is_up)    //If load control is true and is_up control is true
+                    begin
+                        if(counterif.out == 2**$bits(counterif.out)-1) //If counter output reaches its maximum 
+                            counterif.out <= counterif.in;             //Load value
+                        else
+                            counterif.out <= counterif.out + 1;        //Otherwise increment output
+                        end
+                else if(counterif.load && !counterif.is_up)  //If load control is true and is_up control is false
+                    begin
+                        if(counterif.out == counterif.in)   //If counter output reaches its load value
+                            counterif.out <= 2**$bits(counterif.out)-1; //Load maximum value
+                        else
+                            counterif.out <= counterif.out - 1;     //Otherwise decrement output
+                        end
+                else if(!counterif.load && counterif.is_up) //If load control is false and is_up control is true
+                    counterif.out <= counterif.out + 1;     //Increment output
+                else
+                    counterif.out <= counterif.out - 1;     //Otherwise decrement output
+        end
 endmodule
 
+//Top module declaration 
+module top;
+    bit clk;
+    always #5 clk = ~clk;
 
+    counter_if counterif(clk);
+    counter_with_ifc a1(counterif);
+    counter_tb b1(counterif);
+endmodule : top   
--- UpDownCounter/counter_tb.sv
+++ UpDownCounter/counter_tb.sv
@@ -1,75 +1,53 @@
-module tb_counter;
-  reg clk;                     // Declare an internal TB variable called clk to drive clock to the design
-  reg ctrl;
-  reg rstn;                    // Declare an internal TB variable called rstn to drive active low reset to design
-  wire [2:0] out;              // Declare a wire to connect to design output
+`timescale 1ns / 1ps
 
-  // Instantiate counter design and connect with Testbench variables
-  counter   c0 ( .clk (clk),
-                 .ctrl (ctrl),
-                 .rstn (rstn),
-                 .out (out));
-
-  // Generate a clock that should be driven to design
-  // This clock will flip its value every 5ns -> time period = 10ns -> freq = 100 MHz
-  always #5 clk = ~clk;
-
-  // This initial block forms the stimulus of the testbench
-  initial begin
-    // 1. Initialize testbench variables to 0 at start of simulation
-    clk <= 0;
-    rstn <= 0;
-    ctrl <= 0;
-
-    // 2. Drive rest of the stimulus, reset is asserted in between
-    #200   rstn <= 1;
-
-    #40 ctrl <= 1;
-    #80 ctrl <= 0;
-
-    #200   rstn <= 0;
-
-    // 3. Finish the stimulus after 400ns
+//Testbench declaration
+module counter_tb(counter_if counterif);
+    
+    //Task enabling reset declaration
+    task reset_on(input integer delay);
+      #delay counterif.rst <= 1;
+    endtask 
+    
+    //Task disabling reset declaration
+    task reset_off(input integer delay);
+      #delay counterif.rst <= 0;
+    endtask 
+    
+    //Task enabling counting up declaration
+    task apply_up(input integer delay);
+      #delay  counterif.is_up <= 1;
+    endtask
+    
+    //Task disabling counting up declaration
+    task apply_down(input integer delay);
+      #delay  counterif.is_up <= 0;
+    endtask
+    
+    //Task enabling loading value declaration
+    task apply_load(input integer delay, input integer load_value);
+      #delay  counterif.load <= 1;
+        counterif.in <= load_value;
+    endtask
+    
+    //Task disabling loading value declaration
+    task apply_load_off(input integer delay);
+      #delay  counterif.load <= 0;
+        //counterif.in <= 0;
+    endtask
+    
+    
+    //Simple simulation
+    initial begin
+    
+    reset_on(5);
+    reset_off(0);
+    
+    apply_load(200,2);
+    apply_up(50);
+    apply_load(125,5);
+    apply_down(50);
+    apply_load_off(250);
+    
     #1000 $finish;
   end
 endmodule
-
-
-
-
-
-
-
-
-//// Code your testbench here
-//// or browse Examples
-////testbench
-
-//`timescale 1ns/1ns
-
-//module Counter_4bit_tb;
-
-// logic reset;
-// logic CLK;
-//  logic [3:0] out;
-
-// Counter_4bit TestCounter (	.CLK(CLK),
-//				.reset(reset),
-//				.out(out) );
-
-// initial 
-//  begin
-//   reset <= 0; 
-//   CLK <= 0; 
-//   #10 reset <= 1;
-//   #20 reset <= 0;
-//   #200 $finish;
-//  end
-
-// always 
-//  begin 
-//   CLK <= 1; #5;
-//   CLK <= 0; #5;
-//  end 
-
-//endmodule	
