16cd4e4e1593a6f07e520aadf3f2e2251d297685
Artur Nowak <arturmareknowal@gmail.com>
2020-07-19 02:58:04 +0200

    Added parametrization

--- UpDownCounter/counter.sv
+++ UpDownCounter/counter.sv
@@ -1,52 +1,64 @@
-interface counter_if(input bit clk); //Declaration of counters interface with clock signal as input
-    logic [2:0] in, out;             //Declaration of signal in - value loaded into counter if signal load == 1 and an output register
-    logic rst, load, is_up;          //Declaration of reset signal, load control signal (if load == 1 value is loaded into counter)
-endinterface                         //Declaration of is_up control signal (if is_up == 1 counter counts up)
+interface counter_if #(BIT_WIDTH, CLK_PERIOD); //Declaration of counters interface with clock signal as input and parameters
+    logic [BIT_WIDTH-1:0] in, out;             //Declaration of signal in - value loaded into counter if met conditions, and signal output 
+    logic [1:0] load_value_and_if_is_up;       //Declaration of vector which controls counting up or down and with loading value or without
+    logic clk, rst;                            //Declaration of reset signal and clock signal                    
+endinterface                         
 
 
-module counter_with_ifc(counter_if counterif); //Declaration of counter with an interface
+module counter_with_ifc #(BIT_WIDTH, CLK_PERIOD) (counter_if counterif); //Declaration of counter with an interface
+    
+    //Signals initial values
+    initial 
+    begin : starting_state             
+        counterif.rst <= 0;
+        counterif.load_value_and_if_is_up = 2'b00;
+        counterif.in <= 0;
+        counterif.out <= 0;
 
-initial begin              //Signals initial values
-    counterif.rst <= 0;
-    counterif.load <= 0;
-    counterif.in <= 0;
-    counterif.out <= 0;
-    counterif.is_up <= 0;
-end
-
+    end : starting_state  
+    
     //Modeling sequntional flipflop logic
-    always_ff @(posedge counterif.clk or posedge counterif.rst)
-        begin
-            if(counterif.rst)    //If reset is on outpu equals 0 
+    always_ff @(posedge counterif.clk)
+        begin : is_reset
+        
+            if(counterif.rst)    //If reset is on then the output equals 0 
                 counterif.out <= '0;
             else
-                if(counterif.load && counterif.is_up)    //If load control is true and is_up control is true
-                    begin
-                        if(counterif.out == 2**$bits(counterif.out)-1) //If counter output reaches its maximum 
-                            counterif.out <= counterif.in;             //Load value
-                        else
-                            counterif.out <= counterif.out + 1;        //Otherwise increment output
-                        end
-                else if(counterif.load && !counterif.is_up)  //If load control is true and is_up control is false
-                    begin
-                        if(counterif.out == counterif.in)   //If counter output reaches its load value
-                            counterif.out <= 2**$bits(counterif.out)-1; //Load maximum value
-                        else
-                            counterif.out <= counterif.out - 1;     //Otherwise decrement output
-                        end
-                else if(!counterif.load && counterif.is_up) //If load control is false and is_up control is true
-                    counterif.out <= counterif.out + 1;     //Increment output
-                else
-                    counterif.out <= counterif.out - 1;     //Otherwise decrement output
-        end
+            begin : configuration_of_machine
+                case (counterif.load_value_and_if_is_up)
+                    
+                    2'b00: counterif.out <= counterif.out - 1; //If vector eqauls 00 then counter counts down without load
+                       
+                    2'b01: counterif.out <= counterif.out + 1; //If vector eqauls 01 then counter counts up without load
+                    
+                    //If vector eqauls 10 then counter counts down with load   
+                    2'b10: if(counterif.out == counterif.in)   //If counter output reaches its load value
+                                counterif.out <= 2**BIT_WIDTH-1; //Load maximum value
+                           else
+                                counterif.out <= counterif.out - 1;     //Otherwise decrement output
+                    
+                    //If vector eqauls 11 then counter counts up with load   
+                    2'b11: if(counterif.out == 2**BIT_WIDTH-1) //If counter output reaches its maximum 
+                               counterif.out <= counterif.in;             //Load value
+                           else
+                               counterif.out <= counterif.out + 1;        //Otherwise increment output
+                    
+                    default: counterif.out <= counterif.out - 1;          //Counter counts down without load by default
+                
+                endcase
+            end : configuration_of_machine
+        end : is_reset
 endmodule
 
 //Top module declaration 
 module top;
-    bit clk;
-    always #5 clk = ~clk;
+    
+    //Parameters declaration
+    parameter BIT_WIDTH = 4, CLK_PERIOD = 20;
 
-    counter_if counterif(clk);
-    counter_with_ifc a1(counterif);
-    counter_tb b1(counterif);
+    counter_if #(.BIT_WIDTH(BIT_WIDTH), .CLK_PERIOD(CLK_PERIOD)) counterif();
+    
+    counter_with_ifc #(.BIT_WIDTH(BIT_WIDTH), .CLK_PERIOD(CLK_PERIOD)) counter_design (counterif);
+    counter_tb #(.BIT_WIDTH(BIT_WIDTH), .CLK_PERIOD(CLK_PERIOD)) tb (counterif);
+
 endmodule : top   
