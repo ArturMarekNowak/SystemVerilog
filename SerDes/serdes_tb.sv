7f56dd78f2191a0f4dc6e99b52ff3f05459fd06e
Artur Nowak <arturmareknowal@gmail.com>
2020-07-21 14:34:19 +0200

    first tests of strobe signal

--- SerDes/serdes_tb.sv
+++ SerDes/serdes_tb.sv
@@ -8,7 +8,7 @@ logic [2:0] ptime;
 
 //Declaration of parameters
 parameter CLK_600_PERIOD = 1.67;
-parameter CLK_300_PERIOD = 3.33;
+parameter CLK_300_PERIOD = 2*CLK_600_PERIOD;
 parameter BIT_TIME_WIDTH_1 = 0.4;
 parameter BIT_TIME_WIDTH_2 = 5;
 parameter BIT_TIME_WIDTH_3 = 15;
@@ -38,7 +38,6 @@ pin_capt dut(    .pin_in (pin_in),
     
         clk600_90 = 1;
         #(CLK_600_PERIOD/4) clk600_90 = 0;
-        
         forever #(CLK_600_PERIOD/2) clk600_90 = ~clk600_90 ;
         
     end : clock_600MHz_phase_shifted
@@ -48,6 +47,7 @@ pin_capt dut(    .pin_in (pin_in),
     begin : clock_300MHz
     
         clk300 = 0;
+        #(CLK_600_PERIOD/2) clk300 = 1;
         forever #(CLK_300_PERIOD/2) clk300 = ~clk300 ;
         
     end : clock_300MHz
@@ -63,11 +63,39 @@ pin_capt dut(    .pin_in (pin_in),
     task ones_and_zeros();
       forever #BIT_TIME_WIDTH_1 pin_in = ~pin_in;
     endtask 
+    
+    
+    task generating_bits(input integer number_of_bits, input integer bit_length);
+    
+        #(6.5*CLK_600_PERIOD+CLK_600_PERIOD/8) pin_in = 1;
+        #(1*bit_length*CLK_600_PERIOD/4) pin_in = 0;
+        
+        
+        for(int i = 1; i < number_of_bits; i++)
+        begin : loop
+            #(10*CLK_600_PERIOD+2*(CLK_600_PERIOD/8)-1*bit_length*CLK_600_PERIOD/4) pin_in = 1;
+            #(1*bit_length*CLK_600_PERIOD/4) pin_in = 0;
+        end
+        
+    endtask
      
     initial 
     begin : simulation
-    
-        random_bits();
+        
+        pin_in = 0;
+        
+        //random_bits();
+//        #(7.5*CLK_600_PERIOD-0.9) pin_in = 1;
+//        #(3*CLK_600_PERIOD/4) pin_in = 0;
+        
+//        #(2*CLK_600_PERIOD-0.9) pin_in = 1;
+//        #(1*CLK_600_PERIOD/4) pin_in = 0;
+        
+//        #(1*CLK_600_PERIOD-0.9) pin_in = 1;
+//        #(1*CLK_600_PERIOD/4) pin_in = 0;
+        
+        generating_bits(30,20);
+        
         
         #1000 $finish;
     end : simulation
